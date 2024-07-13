b322ab23cc6ff9a96b5b85065c83dbc861598278
aashu <aashu@Coderlaptop>
2024-07-13 15:30:48 +0530

    commit

--- GITMavenProject01/src/main/java/com/nt/service/Arithmetic.java
+++ GITMavenProject01/src/main/java/com/nt/service/Arithmetic.java
@@ -2,4 +2,11 @@ package com.nt.service;
 
 public class Arithmetic {
 
+	
+	public int sum(int x, int y)
+	{
+		return x+y;
+	}
+	
+	
 }
--- GITMavenProject01/src/test/java/com/nt/test/AppTest.java
+++ GITMavenProject01/src/test/java/com/nt/test/AppTest.java
@@ -1,5 +1,22 @@
 package com.nt.test;
 
+import org.junit.jupiter.api.Test;
+
+import com.nt.service.Arithmetic;
+
 public class AppTest {
 
+	@Test
+	public void testPositives() {
+		 Arithmetic ar =new  Arithmetic();
+		 
+		 int expected = 40;
+		 
+		 int actual = ar.sum(expected, expected);
+	
+	
+	}
+	
+	
+	
 }
