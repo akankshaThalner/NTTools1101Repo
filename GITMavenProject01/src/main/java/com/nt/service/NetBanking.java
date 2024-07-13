7bdf7cb06f7604dd000fa9699b243728de7f8fad
aashu <aashu@Coderlaptop>
2024-07-13 17:38:51 +0530

    bharat pe

--- GITMavenProject01/src/main/java/com/nt/service/NetBanking.java
+++ GITMavenProject01/src/main/java/com/nt/service/NetBanking.java
@@ -6,8 +6,16 @@ public class NetBanking {
 	public String netBanking(String uname , String pass) {
 	return "netbanking is added";	
 	}
+
 	public String paypall(String url) {
 		return "paypall integration is done";
+
+}
+	public String enableBharatPe() {
+		//bharat pe configuration is going on
+		
+		return "bharat pe integration is done";
+
 	}
 	
 	
