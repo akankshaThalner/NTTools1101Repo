0ce4079609e23e1340c7e7af77a1840fade477ca
akankshaThalner <165530467+akankshaThalner@users.noreply.github.com>
2025-03-02 22:22:38 +0530

    Update ApplicationRun.java

--- GITMavenProject01/src/main/java/com/nt/main/ApplicationRun.java
+++ GITMavenProject01/src/main/java/com/nt/main/ApplicationRun.java
@@ -8,9 +8,7 @@ public class ApplicationRun {
 	public static void main(String[] args) {
 		
 		
-		 Arithmetic  ar = new  Arithmetic();
-		 
-		 System.out.println("sum is :"+ ar.sum(10,20));
+		
 		 
 	}
 }
