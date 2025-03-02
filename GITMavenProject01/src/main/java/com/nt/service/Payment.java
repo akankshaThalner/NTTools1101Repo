1ae810fb0ffe06dda47bafe00ed08a9c77091aee
akankshaThalner <165530467+akankshaThalner@users.noreply.github.com>
2025-03-02 22:23:26 +0530

    Update Payment.java

--- GITMavenProject01/src/main/java/com/nt/service/Payment.java
+++ GITMavenProject01/src/main/java/com/nt/service/Payment.java
@@ -3,16 +3,7 @@ package com.nt.service;
 public class Payment {
 
 	
-	
-	public String doCarPayment() {
-		return "Do Card Payment is done";
-		
-	}
-	
-	
-	public String doUPIPayment() {
-		return "UPI Payment is done";
-	}
+
 	
 	
 }
